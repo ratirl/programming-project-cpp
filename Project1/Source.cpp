2e361d20489b1603184c8a2dfb6ef733a8a23496
Ali bamboo <sjokz@hotmail.com>
2019-08-24 22:48:48 +0200

    bcrypt\blowfish klasse toegevoegt

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -5,10 +5,12 @@ using namespace mysqlx;
 
 int main()
 {
-	Session session = Database::getSession();
-	Schema schema = session.getSchema("1819IP_groep14");
-	Table table = schema.getTable("RATIRL");
-	table.insert("id", "name").values(1, "testinput").execute();
+	//Session session = Database::getSession();
+	//Schema schema = session.getSchema("1819IP_groep14");
+	//Table table = schema.getTable("RATIRL");
+	//table.insert("id", "name").values(1, "testinput").execute();
+
+
 	cout << "Test om te zien of github werkt";
 	return 0;
 }
