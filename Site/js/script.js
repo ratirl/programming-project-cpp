6426f816925a35d004af164851ebc7e084000695
Ali bamboo <sjokz@hotmail.com>
2019-08-25 21:42:35 +0200

    login pagina die redirect obv user klaar

--- Site/js/script.js
+++ Site/js/script.js
@@ -1,21 +1,43 @@
 $(function(){
     console.log('linked');
-    //default setup
-    $('form').hide();
-    $('#listOfBooks').show();
+    //login ajax call
+    $('#form_login').submit(function (e) {
+        //standard behaviour block
+        e.preventDefault();
+
+        //data terug krijgen vd form met jquery
+        let loginObject = {
+            email: $('#emailinput').val(),
+            password: $('#passwordinput').val(),
+        };
+
+        //Call to server
+        $.ajax({
+            url: 'http://127.0.0.1:3000/checklogin',
+            method: 'POST',
+            data: loginObject
+
+        }).done(function (data) {
+            console.log('login gecheckt!');
+            console.log(data);
+            if (data == 'admin'){
+                window.location.href = "admin.html";
+            } else if (data == 'koerier'){
+                window.location.href = "koerierhomepage.html";
+            } else if (data == 'klant') {
+                window.location.href = "klanthomepage.html";
+            } else if (data == 'fouteLogin'){
+                $('#errorMsg').css("visibility", "visible");
+            }
+          //  Cookies.set('login_xd', login_id);
+           // Cookies.set('login_voornaam', data[0].voornaam);
+            console.log('set id to cookie value werkte')
 
 
-    //Eventlisteners voor buttons
-    $('#list').click(function(){
-        $('form').hide();
-        $('#listOfBooks').show();
+        }).fail(function (er1, er2) {
+            console.log(er1);
+            console.log(er2);
+        });
     });
-    $('#form').click(function(){
-        $('form').show();
-        $('#listOfBooks').hide();
-    });
-
-
-
 
 });
--- Site/node/server.js
+++ Site/node/server.js
@@ -2,16 +2,33 @@ const express = require('express');
 const mysql = require('mysql2');
 const config = require('./config')
 const path = require('path');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 //Page to be redirecten wnr ge localhost:3000 intyped
 //Basically index of homepage
 app.get('/', (req, res) => {
 
-    res.sendFile(path.join(__dirname + '/html/login.html'));
+    res.sendFile(path.join(__dirname + '/html/help.html'));
 
 });
+
+//Middleware
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+//Bodyparser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
 
 //Connection to database
 const connection = mysql.createConnection({
@@ -32,6 +49,48 @@ app.get('/getPakketten', (req, res) => {
                 res.send(results);
             }
         }
+    );
+});
+
+//POST register //eventjes form testen
+/* app.get('/register', (req, res) => {
+    console.log('register triggered!');
+    console.log(req.body);
+
+    bcrypt.hash("admin", saltRounds, function (err, hash) {
+        connection.query(
+            'INSERT INTO `L9_Login` (email, password, type) VALUES (?, ?, ?)',
+            ["ali.trabi@student.ehb.be", hash, "admin"],
+            function (err, results) {
+                console.log(results);
+                console.log(err);
+            }
+        );
+    });
+
+    res.send('OK');
+}); */
+
+//POST login checken en doorsturen indien true
+app.post('/checklogin', (req, res) => {
+    console.log('login check triggered!');
+    connection.query(
+        'SELECT `email`, `password`, `type` FROM `L9_Login` WHERE `email` = ?', [req.body.email],
+        function (err, results, fields) {
+            if (results.length > 0) { 
+            bcrypt.compare(req.body.password, results[0].password, function (err, ress) {
+                if (ress) {
+                    console.log("juiste password");
+                    //als we resultaat hebben dan sturen we de type terug om op basis daarvan
+                    // naar de juiste inlog pagina te redirecten
+                    res.send(results[0].type);
+                } else {
+                    console.log("verkeerde password");
+                    res.send("fouteLogin");
+                }
+            });
+        };
+        } 
     );
 });
 
