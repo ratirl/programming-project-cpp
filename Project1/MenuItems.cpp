93f41788ef60e2f62941ceff052c8d41ca7e0138
Ali bamboo <sjokz@hotmail.com>
2019-08-26 18:40:38 +0200

    menu items zijn opgevult, enkel implementatie nog

--- Project1/MenuItems.cpp
+++ Project1/MenuItems.cpp
@@ -4,21 +4,120 @@ int MenuItems::hoofdMenu()
 {
 	int keuze;
 	do {
-		cout << "    Welkom.    " << endl << endl;
+		cout << "    | hoofdmenu |    " << endl << endl;
 		cout << "    1. voertuigen inbrengen" << endl;
 		cout << "    2. bestellingen inbrengen" << endl;
 		cout << "    3. lijst voertuig met lading" << endl;
 		cout << "    4. detals voertuig" << endl;
-		cout << "    5. statistieken lijsten" << endl << endl;
+		cout << "    5. statistieken lijsten" << endl;
 		cout << "    6. detals pakket" << endl;
-		cout << "    0. sluitenn" << endl << endl;
+		cout << "    0. sluiten" << endl << endl;
 		cout << "    type je keuze in: ";
 		cin >> keuze;
 		cout << endl;
 
 		if ((keuze < 0) || (keuze > 6)) {
-			cout << "    Ongeldige invoer, kies openieuw." << endl << endl;
+			cout << "    ongeldige invoer, kies openieuw." << endl << endl;
 		}
 	} while (((keuze < 0) || (keuze > 6)));
+	return keuze;
+}
+
+int MenuItems::voertuigenInbrengen() {
+	int keuze;
+	do {
+		cout << "    1. voertuig inbrengen" << endl;
+		cout << "    2. voertuig archiveren" << endl; //actief naar 0 zetten in de database
+		cout << "    0. terug" << endl;
+		cout << "    type je keuze in: ";
+		cin >> keuze;
+		cout << endl;
+		if ((keuze < 0) || (keuze > 2)) {
+			cout << "    ongeldige invoer, kies openieuw." << endl << endl;
+		}
+	} while ((keuze < 0) || (keuze > 2));
+	return keuze;
+}
+
+int MenuItems::bestellingenInbrengen() {
+	int keuze;
+	do {
+		cout << "    1. bestelling inbrengen" << endl; //voertuig moet ook een status hebben
+		cout << "    2. archiveren archiveren" << endl; //actief naar 0 zetten in de database
+		cout << "    0. terug" << endl;
+		cout << "    type je keuze in: ";
+		cin >> keuze;
+		cout << endl;
+		if ((keuze < 0) || (keuze > 2)) {
+			cout << "    ongeldige invoer, kies openieuw." << endl << endl;
+		}
+	} while ((keuze < 0) || (keuze > 2));
+	return keuze;
+}
+
+int MenuItems::lijstVoertuigLading() {
+	int keuze;
+	do {
+		cout << "    1. vul voertuigen" << endl; //vul enkel de voertuigen die atm status hebben van niet bezet
+		cout << "    2. overzicht voertuigen met lading" << endl; //overzicht tonen welke voertuigen met welke paketten gevult moeten worden
+		cout << "    0. terug" << endl;
+		cout << "    type je keuze in: ";
+		cin >> keuze;
+		cout << endl;
+		if ((keuze < 0) || (keuze > 2)) {
+			cout << "    ongeldige invoer, kies openieuw." << endl << endl;
+		}
+	} while ((keuze < 0) || (keuze > 2));
+	return keuze;
+}
+
+int MenuItems::voertuigDetails() {
+	int keuze;
+	do {
+		//toont initieel lijst val alle voertuigen met hun bijhorend id, user moet dan id ingeven voor meer details
+		cout << "    1. toon status en paketten van voertuig x" << endl; //user geeft id in vd voertuig
+		cout << "    2. editeer status van voertuig x" << endl; //update status vd voertuig
+		cout << "    3. editeer pakket van voertuig x" << endl; //update pakket details vd voertuig
+		cout << "    0. terug" << endl;
+		cout << "    type je keuze in: ";
+		cin >> keuze;
+		cout << endl;
+		if ((keuze < 0) || (keuze > 3)) {
+			cout << "    ongeldige invoer, kies openieuw." << endl << endl;
+		}
+	} while ((keuze < 0) || (keuze > 3));
+	return keuze;
+}
+
+int MenuItems::statistiekenLijsten() {
+	int keuze;
+	do {
+		cout << "    1. opvragen status van paket x" << endl;
+		cout << "    2. overzicht leveringen per gemeente" << endl;
+		cout << "    0. terug" << endl;
+		cout << "    type je keuze in: ";
+		cin >> keuze;
+		cout << endl;
+		if ((keuze < 0) || (keuze > 2)) {
+			cout << "    ongeldige invoer, kies openieuw." << endl << endl;
+		}
+	} while ((keuze < 0) || (keuze > 2));
+	return keuze;
+}
+
+int MenuItems::pakketDetails() {
+	int keuze;
+	do {
+		cout << "    1. opvragen status van pakket x" << endl;
+		cout << "    2. updaten status van pakket x" << endl;
+		cout << "    3. editeer pakket van voertuig x" << endl;
+		cout << "    0. terug" << endl;
+		cout << "    type je keuze in: ";
+		cin >> keuze;
+		cout << endl;
+		if ((keuze < 0) || (keuze > 3)) {
+			cout << "    ongeldige invoer, kies openieuw." << endl << endl;
+		}
+	} while ((keuze < 0) || (keuze > 3));
 	return keuze;
 }
